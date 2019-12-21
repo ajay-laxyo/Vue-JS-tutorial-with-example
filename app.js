4d502aadf57d30ce328dff34515bc99878b6feb9
ajay <ajay@laxyo.org>
2019-12-21 17:17:20 +0530

    Vue JS tutorial with example

---
+++ app.js
@@ -0,0 +1,33 @@
+var myview = new Vue({
+	el:"#our-app",
+	data:{
+		fname:"Ajay", 
+		addr:"Laxyo House mahalakshmi nagar Indore",
+
+		name:"YouTube", // lecture 3 start from here data binding 1st
+		ulink:"https://www.youtube.com",
+		title:"this is youtube link",
+		imageLink:"avt.png",
+		isActive:"1", 
+		textName:"Ajay",
+		textEmail:"ajay@laxyo.org",
+		paragraph:"<p> This is my paragraph set from JS file </p>"
+
+	},
+	// new Vue called new instance
+	// lecture2 methods
+
+	methods:{
+		run : function(message, name){
+			return "I am running "+message +" and my name is :"+ name;
+		},
+		welcome : function(dayTime){
+			return "Good "+dayTime;
+		},
+		valueAccess : function(){
+			return this.fname+", "+this.addr;
+		}
+		
+
+	}
+});
